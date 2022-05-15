c8ff4c9d6cee400264586e8c0558f2f0cd47fa31
Johannes Fischer <fischer.jh@gmail.com>
2022-05-14 23:33:16 -0600

    Implemented controller to retrieve user records and updated configuration of datatable.

--- rflib/main/default/lwc/rflibUserPermissionAssignmentList/rflibUserPermissionAssignmentList.js
+++ rflib/main/default/lwc/rflibUserPermissionAssignmentList/rflibUserPermissionAssignmentList.js
@@ -1,18 +1,20 @@
 import { api, LightningElement } from 'lwc';
 import { createLogger } from 'c/rflibLogger';
+import getUserPermissionAssignments from '@salesforce/apex/rflib_UserPermAssignmentController.getUserPermissionAssignments';
 
 const logger = createLogger('UserPermissionAssignmentList');
 
 const columns = [
-    { label: 'Name', fieldName: 'name' },
-    { label: 'Email', fieldName: 'email', type: 'email' },
-    { label: 'Phone', fieldName: 'phone', type: 'phone' },
-    { label: 'Profile', fieldName: 'profile' }
+    { label: 'Name', fieldName: 'name', sortable: true },
+    { label: 'Email', fieldName: 'email', type: 'email', sortable: true },
+    { label: 'Phone', fieldName: 'phone', type: 'phone', sortable: true },
+    { label: 'Profile', fieldName: 'profile', sortable: true }
 ];
 
 export default class RflibUserPermissionAssignmentList extends LightningElement {
     @api permissionSetName;
     @api isAssigned;
+    @api title;
 
     data = [];
     columns = columns;
@@ -20,12 +22,20 @@ export default class RflibUserPermissionAssignmentList extends LightningElement 
     // eslint-disable-next-line @lwc/lwc/no-async-await
     async connectedCallback() {
         logger.debug('Initializing component');
-        const data = {
-            name: 'Johannes Fischer',
-            email: 'fischer.jh@gmail.com',
-            phone: '4037143138',
-            profile: 'System Administrator'
+
+        const args = {
+            permSetApiName: this.permissionSetName,
+            shouldBeAssigned: this.isAssigned
         };
-        this.data.push(data);
+
+        logger.debug('Retrieving user info: ' + JSON.stringify(args));
+
+        const _this = this;
+        getUserPermissionAssignments(args)
+            .then((result) => {
+                logger.debug('Users identified={0}', JSON.stringify(result));
+                _this.data = result;
+            })
+            .catch((ex) => logger.error('Failed to retrieve user permission information', ex));
     }
 }
