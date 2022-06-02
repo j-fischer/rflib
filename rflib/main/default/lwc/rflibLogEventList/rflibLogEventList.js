888b798c9794912de8006cd6d136af62a96376cb
Johannes Fischer <fischer.jh@gmail.com>
2022-06-02 17:53:21 -0600

    RFLIB: Added filter input for the Log Level to Log Monitor

--- rflib/main/default/lwc/rflibLogEventList/rflibLogEventList.js
+++ rflib/main/default/lwc/rflibLogEventList/rflibLogEventList.js
@@ -58,6 +58,7 @@ export default class LogEventList extends LightningElement {
     filteredRecordCount;
     eventsToDisplay = [];
 
+    levelSearch;
     contextSearch;
     createdBySearch;
     logMessageSearch;
@@ -89,11 +90,12 @@ export default class LogEventList extends LightningElement {
         logger.debug('Display page with index {0}', this.currentPageIndex);
 
         const filteredEvents =
-            this.contextSearch || this.createdBySearch || this.logMessageSearch
+            this.contextSearch || this.levelSearch || this.createdBySearch || this.logMessageSearch
                 ? this.allEvents.filter(
                       (evt) =>
                           (!this.createdBySearch ||
                               (evt.CreatedById || evt.CreatedById__c).indexOf(this.createdBySearch) > -1) &&
+                          (!this.levelSearch || evt.Log_Level__c.indexOf(this.levelSearch) > -1) &&
                           (!this.contextSearch || evt.Context__c.indexOf(this.contextSearch) > -1) &&
                           (!this.logMessageSearch || evt.Log_Messages__c.indexOf(this.logMessageSearch) > -1)
                   )
@@ -131,6 +133,15 @@ export default class LogEventList extends LightningElement {
         if (this.createdBySearch !== event.target.value) {
             logger.debug('Created by search target={0}', event.target.value);
             this.createdBySearch = event.target.value;
+            this.currentPageIndex = 0;
+            this.refreshEventList();
+        }
+    }
+
+    handleLevelKeyChange(event) {
+        if (this.levelSearch !== event.target.value) {
+            logger.debug('Level search target={0}', event.target.value);
+            this.levelSearch = event.target.value;
             this.currentPageIndex = 0;
             this.refreshEventList();
         }
