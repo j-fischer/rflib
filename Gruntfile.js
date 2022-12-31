83701978e0b6bc66e2b44f629110cd3719bf0d52
Johannes Fischer <fischer.jh@gmail.com>
2022-12-31 16:31:26 -0700

    Fixed bug in test-package-upgrade task in Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -378,7 +378,7 @@ module.exports = function(grunt) {
                     'sfdx force:package:beta:install --package 04t3h000004RdLTAA0 -u <%= config.alias %> -w 10 &&' + //RFLIB@2.6.0-1
                     'sfdx force:package:beta:install --package 04t3h000004jpyMAAQ -u <%= config.alias %> -w 10 &&' + //RFLIB-FS@1.0.2-1
                     'sfdx force:package:beta:install --package 04t3h000004jnfBAAQ -u <%= config.alias %> -w 10 &&' + //RFLIB-TF@1.0.1
-                    'sfdx force:apex:execute -u <%= config.alias %> -f scripts/apex/CreateLogEvent.apex' +
+                    'sfdx force:apex:execute -u <%= config.alias %> -f scripts/apex/CreateLogEvent.apex &&' +
                     'sfdx texei:package:dependencies:install -u <%= config.alias %> --packages <%= config.package.package %> &&' +
                     'sfdx force:package:beta:install --package <%= config.package.latestVersionAlias %> -u <%= config.alias %> -w 10'
             },
