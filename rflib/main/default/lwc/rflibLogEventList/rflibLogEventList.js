d3f6cee84de2fe3c388a0657d555e02b49b88623
Johannes Fischer <fischer.jh@gmail.com>
2024-12-12 13:19:09 -0700

    Added new search box for Request ID and more dynamic UI for search fields in Log Monitor.

--- rflib/main/default/lwc/rflibLogEventList/rflibLogEventList.js
+++ rflib/main/default/lwc/rflibLogEventList/rflibLogEventList.js
@@ -25,100 +25,234 @@
  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
- *
- * This component was inspired by:
- * https://salesforcelightningwebcomponents.blogspot.com/2019/04/pagination-with-search-step-by-step.html
  */
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import { createLogger } from 'c/rflibLogger';
 
 const logger = createLogger('LogEventList');
 
-export default class LogEventList extends LightningElement {
+const SEARCH_FIELDS = {
+    CREATED_BY: 'createdBy',
+    LEVEL: 'level',
+    CONTEXT: 'context',
+    REQUEST_ID: 'requestId',
+    LOG_MESSAGE: 'logMessage'
+};
+
+export default class RflibLogEventList extends LightningElement {
     @api pageSize;
+    @track _currentPage = 1; // Changed to internal tracked property
+    @api permissionRecords;
 
-    @api
-    get currentPage() {
-        return this.currentPageIndex + 1;
-    }
-    set currentPage(value) {
-        this.currentPageIndex = value - 1;
-        this.refreshEventList();
-    }
+    @track allRecords = [];
+    @track filteredRecordCount;
+    @track recordsToDisplay = [];
+    @track selectedRow;
 
-    @api
-    get logEvents() {
-        return this.allEvents;
-    }
-    set logEvents(value) {
-        this.allEvents = value || [];
-        this.refreshEventList();
-    }
+    @track createdBySearch;
+    @track levelSearch;
+    @track contextSearch;
+    @track requestIdSearch;
+    @track logMessageSearch;
+    @track focusedSearchField = null;
 
-    filteredRecordCount;
-    eventsToDisplay = [];
-
-    levelSearch;
-    contextSearch;
-    createdBySearch;
-    logMessageSearch;
+    totalPages = 1;
+    currentPageIndex = 0;
+    displayedPageIndex;
 
     @api
     get title() {
         return this.filteredRecordCount + ' Displayed Log Events';
     }
 
-    totalPages = 1;
-    currentPageIndex = 0;
-    displayedPageIndex;
-    selectedRow;
+    // Computed fields for CSS classes
+    get baseSearchFieldClass() {
+        return 'slds-col search-field';
+    }
 
-    allEvents = [];
-    filteredEvents = [];
+    get expandedSearchFieldClass() {
+        return `${this.baseSearchFieldClass} slds-size_1-of-2 search-field-expanded`;
+    }
 
-    connectedCallback() {
-        logger.debug('Initializing');
+    get collapsedSearchFieldClass() {
+        return `${this.baseSearchFieldClass} slds-size_1-of-12 search-field-collapsed`;
+    }
+
+    get defaultSearchFieldClass() {
+        return `${this.baseSearchFieldClass} slds-size_1-of-6`;
+    }
+
+    get createdByFieldClass() {
+        return this.getFieldClass(SEARCH_FIELDS.CREATED_BY);
+    }
+
+    get levelFieldClass() {
+        return this.getFieldClass(SEARCH_FIELDS.LEVEL);
+    }
+
+    get contextFieldClass() {
+        return this.getFieldClass(SEARCH_FIELDS.CONTEXT);
+    }
+
+    get requestIdFieldClass() {
+        return this.getFieldClass(SEARCH_FIELDS.REQUEST_ID);
+    }
+
+    get logMessageFieldClass() {
+        return this.getFieldClass(SEARCH_FIELDS.LOG_MESSAGE);
+    }
+
+    getFieldClass(fieldName) {
+        if (this.focusedSearchField === null) {
+            return this.defaultSearchFieldClass;
+        }
+        return this.focusedSearchField === fieldName ? this.expandedSearchFieldClass : this.collapsedSearchFieldClass;
+    }
+
+    // Focus handlers for each field
+    handleCreatedByFocus() {
+        this.focusedSearchField = SEARCH_FIELDS.CREATED_BY;
+    }
+
+    handleLevelFocus() {
+        this.focusedSearchField = SEARCH_FIELDS.LEVEL;
+    }
+
+    handleContextFocus() {
+        this.focusedSearchField = SEARCH_FIELDS.CONTEXT;
+    }
+
+    handleRequestIdFocus() {
+        this.focusedSearchField = SEARCH_FIELDS.REQUEST_ID;
+    }
+
+    handleLogMessageFocus() {
+        this.focusedSearchField = SEARCH_FIELDS.LOG_MESSAGE;
+    }
+
+    handleSearchFieldBlur() {
+        // Small delay to handle focus changes between fields
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        setTimeout(() => {
+            const focusedElement = this.template.activeElement;
+            if (!focusedElement || !focusedElement.dataset.field) {
+                this.focusedSearchField = null;
+            }
+        }, 100);
+    }
+
+    handleCreatedByKeyPress(event) {
+        if (event.which === 13) {
+            this.executeSearch();
+        }
+    }
+
+    handleLevelKeyPress(event) {
+        if (event.which === 13) {
+            this.executeSearch();
+        }
+    }
+
+    handleContextKeyPress(event) {
+        if (event.which === 13) {
+            this.executeSearch();
+        }
+    }
+
+    handleRequestIdKeyPress(event) {
+        if (event.which === 13) {
+            this.executeSearch();
+        }
+    }
+
+    handleLogMessageKeyPress(event) {
+        if (event.which === 13) {
+            this.executeSearch();
+        }
+    }
+
+    handleCreatedByChanged(event) {
+        if (this.createdBySearch !== event.target.value) {
+            this.createdBySearch = event.target.value;
+        }
+    }
+
+    handleLevelKeyChange(event) {
+        if (this.levelSearch !== event.target.value) {
+            this.levelSearch = event.target.value;
+        }
+    }
+
+    handleContextKeyChange(event) {
+        if (this.contextSearch !== event.target.value) {
+            this.contextSearch = event.target.value;
+        }
+    }
+
+    handleRequestIdChange(event) {
+        if (this.requestIdSearch !== event.target.value) {
+            this.requestIdSearch = event.target.value;
+        }
+    }
+
+    handleLogMessageKeyChange(event) {
+        if (this.logMessageSearch !== event.target.value) {
+            this.logMessageSearch = event.target.value;
+        }
+    }
+
+    executeSearch() {
+        logger.debug(
+            'Executing search for createdBy={0}, level={1}, context={2}, requestId={3}, logMessage={4}',
+            this.createdBySearch,
+            this.levelSearch,
+            this.contextSearch,
+            this.requestIdSearch,
+            this.logMessageSearch
+        );
+        this.currentPageIndex = 0;
         this.refreshEventList();
     }
 
     refreshEventList() {
-        if (this.selectedRow) {
-            this.selectedRow.classList.remove('selected');
-        }
-
         this.displayedPageIndex = this.currentPageIndex;
         logger.debug('Display page with index {0}', this.currentPageIndex);
 
-        const filteredEvents =
-            this.contextSearch || this.levelSearch || this.createdBySearch || this.logMessageSearch
-                ? this.allEvents.filter(
-                      (evt) =>
+        const filteredRecords =
+            this.createdBySearch ||
+            this.levelSearch ||
+            this.contextSearch ||
+            this.requestIdSearch ||
+            this.logMessageSearch
+                ? this.allRecords.filter(
+                      (rec) =>
                           (!this.createdBySearch ||
-                              (evt.CreatedById || evt.CreatedById__c).indexOf(this.createdBySearch) > -1) &&
-                          (!this.levelSearch || evt.Log_Level__c.indexOf(this.levelSearch) > -1) &&
-                          (!this.contextSearch || evt.Context__c.indexOf(this.contextSearch) > -1) &&
-                          (!this.logMessageSearch || evt.Log_Messages__c.indexOf(this.logMessageSearch) > -1)
+                              (rec.CreatedById || rec.CreatedById__c).indexOf(this.createdBySearch) > -1) &&
+                          (!this.levelSearch || rec.Log_Level__c.indexOf(this.levelSearch) > -1) &&
+                          (!this.contextSearch || rec.Context__c.indexOf(this.contextSearch) > -1) &&
+                          (!this.requestIdSearch || rec.Request_ID__c.indexOf(this.requestIdSearch) > -1) &&
+                          (!this.logMessageSearch || rec.Log_Messages__c.indexOf(this.logMessageSearch) > -1)
                   )
-                : this.allEvents;
+                : this.allRecords;
 
-        this.filteredEvents = filteredEvents.map(function (evt, index) {
+        this.filteredRecords = filteredRecords.map((evt, index) => {
             const modifiedEvt = { ...evt };
-            modifiedEvt.Id = index;
+            modifiedEvt.id = index;
             return modifiedEvt;
         });
-        this.filteredRecordCount = this.filteredEvents.length;
 
-        logger.debug('Total records count {0}', this.filteredRecordCount);
+        this.filteredRecordCount = this.filteredRecords.length;
 
         if (this.filteredRecordCount > 0) {
             this.totalPages = Math.ceil(this.filteredRecordCount / this.pageSize);
-
             const startIndex = this.currentPageIndex * this.pageSize;
-            this.eventsToDisplay = this.filteredEvents.slice(startIndex, startIndex + this.pageSize);
+            this.recordsToDisplay = this.filteredRecords.slice(startIndex, startIndex + this.pageSize);
         } else {
-            this.eventsToDisplay = [];
+            this.recordsToDisplay = [];
             this.totalPages = 1;
         }
+
+        logger.debug('recordsToDisplay={0}', JSON.stringify(this.recordsToDisplay));
 
         const event = new CustomEvent('refreshed', {
             detail: JSON.stringify({
@@ -129,38 +263,29 @@ export default class LogEventList extends LightningElement {
         this.dispatchEvent(event);
     }
 
-    handleCreatedByChanged(event) {
-        if (this.createdBySearch !== event.target.value) {
-            logger.debug('Created by search target={0}', event.target.value);
-            this.createdBySearch = event.target.value;
-            this.currentPageIndex = 0;
-            this.refreshEventList();
-        }
+    connectedCallback() {
+        logger.debug('Initializing');
+        this.refreshEventList();
     }
 
-    handleLevelKeyChange(event) {
-        if (this.levelSearch !== event.target.value) {
-            logger.debug('Level search target={0}', event.target.value);
-            this.levelSearch = event.target.value;
-            this.currentPageIndex = 0;
-            this.refreshEventList();
-        }
+    @api
+    set logEvents(value) {
+        this.allRecords = value || [];
+        this.refreshEventList();
+    }
+    get logEvents() {
+        return this.allRecords;
     }
 
-    handleContextKeyChange(event) {
-        if (this.contextSearch !== event.target.value) {
-            logger.debug('Context search target={0}', event.target.value);
-            this.contextSearch = event.target.value;
-            this.currentPageIndex = 0;
-            this.refreshEventList();
-        }
+    @api
+    get currentPage() {
+        return this._currentPage;
     }
-
-    handleLogMessageKeyChange(event) {
-        if (this.logMessageSearch !== event.target.value) {
-            logger.debug('Log message search target={0}', event.target.value);
-            this.logMessageSearch = event.target.value;
-            this.currentPageIndex = 0;
+    set currentPage(value) {
+        this._currentPage = value;
+        this.currentPageIndex = value - 1;
+        if (this.isConnected) {
+            // Only refresh if component is connected
             this.refreshEventList();
         }
     }
@@ -169,7 +294,7 @@ export default class LogEventList extends LightningElement {
         const currentTarget = event.currentTarget;
 
         const logId = currentTarget.dataset.logId;
-        logger.debug('Log seleceted with Id={0}', logId);
+        logger.debug('Log selected with Id={0}', logId);
 
         currentTarget.classList.add('selected');
 
@@ -178,7 +303,7 @@ export default class LogEventList extends LightningElement {
         }
         this.selectedRow = currentTarget;
 
-        const evtInfo = this.eventsToDisplay.find((evt) => evt.Id === parseInt(logId, 10));
+        const evtInfo = this.recordsToDisplay.find((evt) => evt.id === parseInt(logId, 10));
         const logSelectedEvent = new CustomEvent('logselected', {
             detail: JSON.stringify(evtInfo)
         });
