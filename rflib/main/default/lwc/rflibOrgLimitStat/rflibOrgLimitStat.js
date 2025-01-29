13103fe864f5dc65c4c34f5f8884a75f859568c1
Johannes Fischer <fischer.jh@gmail.com>
2025-01-28 17:22:50 -0700

    Org Limit Stat Component:  Fixed progressbar not displaying the proper state based on the limits retrieved from the server. Also improved UI.

--- rflib/main/default/lwc/rflibOrgLimitStat/rflibOrgLimitStat.js
+++ rflib/main/default/lwc/rflibOrgLimitStat/rflibOrgLimitStat.js
@@ -8,23 +8,36 @@ export default class rflibOrgLimitStat extends LightningElement {
     @api title;
     @api limitName;
 
-    limitProgression = 0;
-    orgLimits = {};
+    _limitProgression = 0;
+    _currentValue = 0;
+    _totalLimit = 0;
+
+    setOrgLimits(newOrgLimits) {
+        this._currentValue = newOrgLimits[this.limitName] ? newOrgLimits[this.limitName].currentValue : 0;
+        this._totalLimit = newOrgLimits[this.limitName] ? newOrgLimits[this.limitName].totalLimit : 0;
+
+        this._limitProgression = this._totalLimit > 0 ? (this._currentValue / this._totalLimit) * 100 : 0;
+    }
 
     get currentValue() {
-        return this.orgLimits[this.limitName] ? this.orgLimits[this.limitName].currentValue : 0;
+        return this._currentValue;
     }
 
     get totalLimit() {
-        return this.orgLimits[this.limitName] ? this.orgLimits[this.limitName].totalLimit : 0;
+        return this._totalLimit;
+    }
+
+    get limitProgression() {
+        return this._limitProgression;
     }
 
     refresh() {
         logger.debug('Refreshing stat: ' + this.limitName);
+
         let _this = this;
         getOrgLimits().then((results) => {
             logger.debug('Org Limits Result: ' + JSON.stringify(results));
-            _this.orgLimits = results;
+            _this.setOrgLimits(results);
         });
     }
 
